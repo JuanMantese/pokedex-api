ed04613670b1b27cb70548d69b813ac84625a14c
Juan Mantese <juanmantese97@gmail.com>
2024-03-03 22:15:52 -0300

    CORS config

--- src/controllers/pokemons.js
+++ src/controllers/pokemons.js
@@ -6,6 +6,9 @@ const {
 const getList = async (req, res) => {  
   try {
     const pokemonData = await getAllPokemons();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.send({ data: pokemonData });
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
@@ -15,6 +18,9 @@ const getList = async (req, res) => {
 const getDetail = async (req, res) => {
   try {
     const pokemonData = await getPokemonDetail(req);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.send({ data: pokemonData });
   } catch (error) {
     res.status(404).json({ error: 'Pokémon not found' });
